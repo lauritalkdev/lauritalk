1abb73d1bd3b224a03ce82d43f0d10f4abc141e6
lauritalkdev <lauritalkdev@gmail.com>
2026-01-18 02:14:04 +0100

    Update package name and icons

--- android/app/src/main/java/com/luminix/lauritalk/MainActivity.kt
+++ android/app/src/main/java/com/luminix/lauritalk/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.lauritalk
+package com.luminix.lauritalk
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/luminix/lauritalk/MainApplication.kt
+++ android/app/src/main/java/com/luminix/lauritalk/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.lauritalk
+package com.luminix.lauritalk
 
 import android.app.Application
 import android.content.res.Configuration
